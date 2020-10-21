9170d94b800bc3bf2b0bb5c1e507cc1b8ee30853
blueblackzebra <sparshkasana99@gmail.com>
2020-10-22 03:36:31 +0530

    Research paper template

--- src/routers/main.js
+++ src/routers/main.js
@@ -31,4 +31,22 @@ router.get('/resources', (req, res) => {
     });
 })
 
+router.get('/paper1', (req, res) => {
+    res.render('paper', {
+        paperTitle: "NLP paper",
+        authorList: "PersonA PersonB",
+        university: "UniA",
+        abstractText: "Abstract for NLP paper"
+    });
+})
+
+router.get('/paper2', (req, res) => {
+    res.render('paper', {
+        paperTitle: "CV paper",
+        authorList: "PersonC PersonD",
+        university: "UniY",
+        abstractText: "Abstract for CV paper"
+    });
+})
+
 module.exports=router;
